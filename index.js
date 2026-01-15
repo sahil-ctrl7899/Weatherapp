ed096a1eccbd287164bf8d00c4f7aec7ca37c74b
Sahil Talreja <Talrejasahil4994@gmail.com>
2026-01-15 16:59:48 +0530

    version 2

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 
 const data =  async (city) => {
-     const result=await fetch(`own weather api link please`);
+     const result=await fetch(``);
      return await result.json();
 } 
+
+const arr=[];
  const button=document.getElementById("button");
  const input=document.querySelector("input");
-
+ const add=document.getElementById("btn2")
  const show = (city, region, country, temperature) => {
     const output = document.getElementById("output");
+        const output2 = document.getElementById("output2");
+    arr.push({name:city,region:region,country:country,temperature:temperature})
+output.innerHTML=""
+ arr.map( (val)=> { output.innerHTML += `
+        <h2>${val.name}</h2>
+        <p>${val.region}, ${val.country}</p>
+        <p>Temperature: ${val.temperature}°C</p>
+    `;})
 
-    output.innerHTML = `
-        <h2>${city}</h2>
-        <p>${region}, ${country}</p>
-        <p>Temperature: ${temperature}°C</p>
-    `;
 };
+
+
 button.addEventListener("click", async () => {
     const city = input.value.trim();
     if (!city) return;
@@ -28,6 +35,11 @@ button.addEventListener("click", async () => {
         res.location.country,
         res.current.temp_c
     );
-
+    
+    
     input.value = "";
+
+
 });
+
+
